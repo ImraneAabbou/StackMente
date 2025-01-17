888b60783567d1a566a33beab8efc7a2375f65b8
Imrane Aabbou <imrane.aabbou@gmail.com>
2025-01-17 03:53:13 +0100

    refactor: sync views method rate limited

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -5,6 +5,8 @@ namespace App\Services;
 use App\Enums\PostType;
 use App\Models\Post;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 
 /**
  * Class PostService
@@ -14,14 +16,29 @@ class PostService
 {
     public function __construct(
         private Post $post,
+        private Request $request
     ) {}
 
     /*
-     * Increment views of a post
+     * Increments the view if first time or key gets decayed
+     * Returns either the view of the given request
+     * was counted or not
+     *
+     * @param $request Request
+     * @return bool
      */
-    public function incrementViews(): void
+    public function syncViews(Request $request): bool
     {
-        $this->post->update(['views' => $this->post->views + 1]);
+        $throttleKey = 'post_' . $this->post->id;
+        $throttleKey .= 'viewer_' . ($request->user()?->email ?? $request->ip());
+
+        // Count only 1 view per 10 mins on same post
+        return RateLimiter::attempt(
+            $throttleKey,
+            1,
+            fn() => $this->post->increment('views'),
+            10 * 60
+        );
     }
 
     /**
