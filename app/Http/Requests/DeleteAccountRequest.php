3266cbdb2cde29e9b1d7ed4251b8049a43c9c228
Imrane Aabbou <imrane.aabbou@gmail.com>
2025-01-19 15:40:29 +0100

    feat: deletion request added

---
+++ app/Http/Requests/DeleteAccountRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DeleteAccountRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $userHasPassword = !!$this->user()?->password;
+
+        return [
+            'password' => [$userHasPassword ? 'required' : 'nullable', 'current_password'],
+        ];
+    }
+}
