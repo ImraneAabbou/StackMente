8c728096a026f2b5a99f40af07257b8ce94c3c5e
Imrane Aabbou <imrane.aabbou@gmail.com>
2025-02-02 21:57:48 +0100

    feat: configure tailwindcss

--- storage/framework/views/1bed27eb7190e6b3cd1326bb705051f2.php
+++ storage/framework/views/1bed27eb7190e6b3cd1326bb705051f2.php
@@ -1,23 +1,32 @@
 <!DOCTYPE html>
-<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>"
+    class="bg-background-light text-text-dark dark:text-text-light dark:bg-background-dark">
 
-        <title inertia><?php echo e(config('app.name', 'Laravel')); ?></title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title inertia><?php echo e(config('app.name', 'Laravel')); ?></title>
 
-        <!-- Scripts -->
-        <?php echo app('Tighten\Ziggy\BladeRouteGenerator')->generate(); ?>
-        <?php echo app('Illuminate\Foundation\Vite')->reactRefresh(); ?>
-        <?php echo app('Illuminate\Foundation\Vite')(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"]); ?>
-        <?php if (!isset($__inertiaSsrDispatched)) { $__inertiaSsrDispatched = true; $__inertiaSsrResponse = app(\Inertia\Ssr\Gateway::class)->dispatch($page); }  if ($__inertiaSsrResponse) { echo $__inertiaSsrResponse->head; } ?>
-    </head>
-    <body class="font-sans antialiased">
-        <?php if (!isset($__inertiaSsrDispatched)) { $__inertiaSsrDispatched = true; $__inertiaSsrResponse = app(\Inertia\Ssr\Gateway::class)->dispatch($page); }  if ($__inertiaSsrResponse) { echo $__inertiaSsrResponse->body; } else { ?><div id="app" data-page="<?php echo e(json_encode($page)); ?>"></div><?php } ?>
-    </body>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+        rel="stylesheet">
+
+    <!-- Scripts -->
+    <?php echo app('Tighten\Ziggy\BladeRouteGenerator')->generate(); ?>
+    <?php echo app('Illuminate\Foundation\Vite')->reactRefresh(); ?>
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"]); ?>
+    <?php if (!isset($__inertiaSsrDispatched)) { $__inertiaSsrDispatched = true; $__inertiaSsrResponse = app(\Inertia\Ssr\Gateway::class)->dispatch($page); }  if ($__inertiaSsrResponse) { echo $__inertiaSsrResponse->head; } ?>
+</head>
+
+<body class="font-sans antialiased">
+    <?php if (!isset($__inertiaSsrDispatched)) { $__inertiaSsrDispatched = true; $__inertiaSsrResponse = app(\Inertia\Ssr\Gateway::class)->dispatch($page); }  if ($__inertiaSsrResponse) { echo $__inertiaSsrResponse->body; } else { ?><div id="app" data-page="<?php echo e(json_encode($page)); ?>"></div><?php } ?>
+</body>
+
 </html>
 <?php /**PATH /home/imrane/Projects/StackMente/resources/views/app.blade.php ENDPATH**/ ?>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,53 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.tsx',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.tsx",
     ],
-
+    darkMode: "selector",
     theme: {
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: "1rem",
+                sm: "2rem",
+            },
+        },
+        colors: {
+            blue: {
+                DEFAULT: "#158BEA",
+                ...colors.blue,
+            },
+            green: {
+                DEFAULT: "#54BF59",
+                ...colors.green,
+            },
+            gray: {
+                DEFAULT: "#9B9B9B",
+                ...colors.gray,
+            },
+            text: {
+                light: "#f4f4f4",
+                dark: "#000000",
+            },
+            warn: "#ccc500",
+            error: "#f31616",
+            success: "#54BF59",
+            background: {
+                light: "#f3f3f5",
+                dark: "#21212C",
+            },
+        },
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Inter", ...defaultTheme.fontFamily.sans],
+                display: ["Playfair Display", ...defaultTheme.fontFamily.serif]
             },
         },
     },
-
-    plugins: [forms],
 };
