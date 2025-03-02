b4e095554b7aba56761ee1c723f95c0e2ac94200
Imrane Aabbou <imrane.aabbou@gmail.com>
2025-03-02 14:32:11 +0000

    feat: auth middle ware to routes that requires auth

--- routes/web.php
+++ routes/web.php
@@ -14,55 +14,61 @@ use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'Index');
 
-Route::put('/posts/{post:slug}', [PostController::class, 'update'])->name('posts.update');
-
-Route::inertia('/articles/create', 'Posts/CreateArticle')->name('articles.create');
 Route::get('/articles', [PostController::class, 'index'])->name('articles.index');
 Route::get('/articles/{post:slug}', [PostController::class, 'show'])->name('articles.show');
 
-Route::inertia('/subjects/create', 'Posts/CreateSubject')->name('subjects.create');
 Route::get('/subjects', [PostController::class, 'index'])->name('subjects.index');
 Route::get('/subjects/{post:slug}', [PostController::class, 'show'])->name('subjects.show');
 
-Route::inertia('/questions/create', 'Posts/CreateQuestion')->name('questions.create');
 Route::get('/questions', [PostController::class, 'index'])->name('questions.index');
 Route::get('/questions/{post:slug}', [PostController::class, 'show'])->name('questions.show');
 
 Route::get('/posts', [PostController::class, 'index'])->name('feed');
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 
-Route::post('/posts/{reportable}/reports', [PostController::class, 'report'])->name('posts.report');
-Route::delete('/posts/{reportable}/reports', [PostController::class, 'clearReports'])->can('delete', [Report::class]);
+Route::middleware('auth')->group(function () {
+    Route::inertia('/articles/create', 'Posts/CreateArticle')->name('articles.create');
+    Route::inertia('/subjects/create', 'Posts/CreateSubject')->name('subjects.create');
+    Route::inertia('/questions/create', 'Posts/CreateQuestion')->name('questions.create');
 
-Route::post('/posts/{votable}/vote', [PostController::class, 'vote'])->name('posts.vote');
-Route::delete('/posts/{votable}/vote', [PostController::class, 'unvote'])->name('posts.unvote');
+    Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+    Route::put('/posts/{post:slug}', [PostController::class, 'update'])->name('posts.update');
 
-Route::post('/posts/{post:slug}/comments', [CommentController::class, 'store'])->name('comments.store');
-Route::post('/comments/{reportable}/reports', [CommentController::class, 'report'])->name('comments.report');
-Route::delete('/comments/{reportable}/reports', [CommentController::class, 'clearReports'])->can('delete', [Report::class]);
-Route::put('/comments/{comment}/mark', [CommentController::class, 'mark'])->name('comments.mark');
-Route::put('/comments/{comment}', [CommentController::class, 'update'])->name('comments.update');
-Route::delete('/comments/{comment}', [CommentController::class, 'destroy']);
+    Route::post('/posts/{reportable}/reports', [PostController::class, 'report'])->name('posts.report');
+    Route::delete('/posts/{reportable}/reports', [PostController::class, 'clearReports'])->can('delete', [Report::class]);
 
-Route::post('/comments/{votable}/vote', [CommentController::class, 'vote'])->name('comments.vote');
-Route::delete('/comments/{votable}/vote', [CommentController::class, 'unvote'])->name('comments.unvote');
+    Route::post('/posts/{votable}/vote', [PostController::class, 'vote'])->name('posts.vote');
+    Route::delete('/posts/{votable}/vote', [PostController::class, 'unvote'])->name('posts.unvote');
 
-Route::post('/comments/{comment}/replies', [ReplyController::class, 'store'])->name('replies.store');
-Route::post('/replies/{reportable}/reports', [ReplyController::class, 'report'])->name('replies.report');
-Route::delete('/replies/{reportable}/reports', [ReplyController::class, 'clearReports'])->can('delete', [Report::class]);
-Route::put('/replies/{reply}', [ReplyController::class, 'update'])->name('replies.update');
-Route::delete('/replies/{reply}', [ReplyController::class, 'destroy'])->name('replies.delete');
+    Route::post('/posts/{post:slug}/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::post('/comments/{reportable}/reports', [CommentController::class, 'report'])->name('comments.report');
+    Route::delete('/comments/{reportable}/reports', [CommentController::class, 'clearReports'])->can('delete', [Report::class]);
+    Route::put('/comments/{comment}/mark', [CommentController::class, 'mark'])->name('comments.mark');
+    Route::put('/comments/{comment}', [CommentController::class, 'update'])->name('comments.update');
+    Route::delete('/comments/{comment}', [CommentController::class, 'destroy']);
 
-Route::put('/notifications/{id}', [NotificationController::class, 'update'])->name('notifications.update');
-Route::delete('/notifications', [NotificationController::class, 'destroy'])->name('notifications.destroy');
+    Route::post('/comments/{votable}/vote', [CommentController::class, 'vote'])->name('comments.vote');
+    Route::delete('/comments/{votable}/vote', [CommentController::class, 'unvote'])->name('comments.unvote');
 
-Route::inertia('/profile', 'Profile/Index', ['missions' => fn() => Mission::all()])->name('profile.index');
-Route::inertia('/profile/delete', 'Profile/Delete')->name('profile.delete');
-Route::inertia('/profile/settings', 'Profile/Edit')->name('profile.edit');
-Route::put('/profile', [ProfileController::class, "update"])->name('profile.update');
+    Route::post('/comments/{comment}/replies', [ReplyController::class, 'store'])->name('replies.store');
+    Route::post('/replies/{reportable}/reports', [ReplyController::class, 'report'])->name('replies.report');
+    Route::delete('/replies/{reportable}/reports', [ReplyController::class, 'clearReports'])->can('delete', [Report::class]);
+    Route::put('/replies/{reply}', [ReplyController::class, 'update'])->name('replies.update');
+    Route::delete('/replies/{reply}', [ReplyController::class, 'destroy'])->name('replies.delete');
+
+    Route::put('/notifications/{id}', [NotificationController::class, 'update'])->name('notifications.update');
+    Route::delete('/notifications', [NotificationController::class, 'destroy'])->name('notifications.destroy');
+
+    Route::inertia('/profile', 'Profile/Index', ['missions' => fn() => Mission::all()])->name('profile.index');
+    Route::inertia('/profile/delete', 'Profile/Delete')->name('profile.delete');
+    Route::inertia('/profile/settings', 'Profile/Edit')->name('profile.edit');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/sync', fn() => SyncEverything::execute());
+});
+
 Route::get('/profile/{user:username}', [ProfileController::class, 'show'])->name('profile.show');
 Route::post('/profile/{user:username}/ban', [ProfileController::class, 'ban']);
-Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 Route::delete('/profile/{user:username}/ban', [ProfileController::class, 'unban']);
 Route::post('/profile/{reportable}/reports', [ProfileController::class, 'report']);
 Route::delete('/profile/{reportable}/reports', [ProfileController::class, 'clearReports'])->can('delete', [Report::class]);
@@ -70,7 +76,5 @@ Route::delete('/profile/{reportable}/reports', [ProfileController::class, 'clear
 Route::get('/rank', UsersRankingController::class)->name('rank');
 
 Route::get('/tags', [TagController::class, 'index'])->name('tags.index');
-
-Route::get('/sync', fn() => SyncEverything::execute());
 
 require __DIR__ . '/auth.php';
