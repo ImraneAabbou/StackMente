8526fa7dc71d9de14fbea867ee5e455627e3492a
Imrane Aabbou <imrane.aabbou@gmail.com>
2025-03-27 22:49:18 +0000

    feat(post,reply,comment): add deletion action uncommited files

---
+++ database/migrations/2025_01_27_231320_create_delete_deleted_posts_from_notifications_trigger.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::unprepared('DROP TRIGGER IF EXISTS delete_deleted_posts_from_notifications');
+        DB::unprepared('
+            CREATE TRIGGER delete_deleted_posts_from_notifications
+            AFTER DELETE ON posts
+            FOR EACH ROW
+            BEGIN
+                DELETE FROM notifications WHERE JSON_UNQUOTE(JSON_EXTRACT(`data`, \'$."post_id"\')) = OLD.id;
+            END
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::unprepared('DROP TRIGGER IF EXISTS delete_deleted_posts_from_notifications');
+    }
+};
