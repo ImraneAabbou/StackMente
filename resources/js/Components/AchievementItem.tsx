65eeb6e15851e438256aecdd28e1a410705a41a7
Imrane Aabbou <imrane.aabbou@gmail.com>
2025-03-29 11:42:21 +0000

    feat: show mission desc

--- resources/js/Components/AchievementItem.tsx
+++ resources/js/Components/AchievementItem.tsx
@@ -2,7 +2,7 @@ import { Mission } from "@/types/mission";
 import { mission_image } from "@/Utils/helpers/path";
 
 export default function AchievementItem({ mission: m }: { mission: Mission }) {
-    return <div className="flex flex-col gap-2 text-center">
+    return <div className="flex flex-col gap-2 text-center" title={m.description}>
         <img src={mission_image(m.image)} className="size-24 mx-auto" />
         <span className={`font-semibold text-xs`}>
             {m.title}
